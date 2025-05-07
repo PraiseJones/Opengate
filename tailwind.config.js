f7a2dfe6eaf0b4da64080daf6cd3acddb79fc2cd
Praise Jones <94780493+PraiseJones@users.noreply.github.com>
2025-05-07 09:49:20 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ['./public/**/*.html', './src/js/**/*.js'],
+  theme: {
+    extend: {
+      colors: {
+        accent: '#ff6f00',
+      },
+      fontFamily: {
+        heading: ['"Power Grotesk"', 'sans-serif'],
+        sans: ['Roboto', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
